534871c11203b6893ab5d37d8ec168d3d9ed9ab7
flowfordan <flowfordan@gmail.com>
2022-04-04 12:35:23 +0300

    store connect

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BookListItem from "../BookListItem/BookListItem";
-import styles from './BookList.module.css'
+import styles from './BookList.module.css';
+import { connect } from "react-redux";
+import withBookstoreService from "../HOC/withBookstoreService";
+import { booksLoaded } from "../../actions/actions";
+import { bindActionCreators } from "redux";
+import compose from "../../utils/compose";
 
 const BookList = (props) => {
     
 
+    useEffect(
+        () => {
+          // get data
+          const {bookstoreService} = props;
+          const data = bookstoreService.getBooks();
+          console.log(data)
+
+          // dispatch action
+          props.booksLoaded(data)
+        },
+        [])
 
     const {books} = props
 
@@ -23,4 +39,21 @@ const BookList = (props) => {
     )
 }
 
-export default BookList;
+const mapStateToProps = (state) => {
+    return {
+        books: state.books
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        booksLoaded
+    }, dispatch)
+}
+
+
+export default compose (
+    withBookstoreService(),
+    connect (mapStateToProps, mapDispatchToProps)
+)(BookList)
+;
--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -1,20 +1,14 @@
 import React from "react";
 import BookList from "../BookList/BookList";
+import { connect } from "react-redux";
 
 const HomePage = () => {
 
-    const books = [{
-        id: 1,
-        title: 'V chem moya vera',
-        author: 'L.N. Tolstoy'},
-        {id: 2,
-        title: 'Atomic Habits',
-        author: 'J. Clear'}
-];
+    
 
     return(
         <div>
-            <BookList books={books}/>
+            <BookList />
         </div>
     )
 }
